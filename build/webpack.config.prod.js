50221938ccacd6bee753ba1e5d60cabcfc56d891
qingcheng <qingchengcode@qq.com>
2021-03-02 12:20:09 +0800

    update docs

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -18,7 +18,7 @@ module.exports = {
     output: {
         filename: 'index.[hash].js',
         // libraryTarget: 'umd',
-        path: path.resolve(__dirname, "../dist")
+        path: path.resolve(__dirname, "../docs")
     },
     module: {
         rules: [
